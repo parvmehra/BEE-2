9f8edcd3cfabec8ad9282d098b17b37b87ad0928
Parv Mehra <156988867+parvmehra@users.noreply.github.com>
2025-07-17 12:44:05 +0530

    Lecture 7: File I/O and Combining Data

--- lec_07/read/index.js
+++ lec_07/read/index.js
@@ -1,7 +1,22 @@
 const fs = require('fs');
+const{ read } = require('./IO/io.js');
+const{ write } = require('./IO/io.js');
 
-fs.readFile('../users.txt',"utf-8", function(err, data) {
-    if(err) return console.log(err);
-    let users = JSON.parse(data);
-    console.log(users[0]);
-});
+
+// fs.readFile('../users.txt',"utf-8", function(err, data) {
+//     if(err) return console.log(err);
+//     let users = JSON.parse(data);
+//     console.log(users[0]);
+// });
+
+
+
+
+async function readuser(){
+    let users = await read("./users.txt");
+    let users2 = await read("./users2.txt");
+    let allusers=users.concat(users2);
+    write("./allusers.txt",allusers);
+
+}
+readuser()
