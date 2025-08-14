695c0f17a4760e75a891653de6be5ef5968c2a85
Parv Mehra <156988867+parvmehra@users.noreply.github.com>
2025-08-14 14:30:49 +0530

    Lecture 16 and 17

---
+++ lec_16/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.post('/user', (req, res) => {
+    const {email,password} = req.body;
+
+    console.log(email,password);
+    try {
+        res.json({
+        success:true,
+        message:'user added successfully',
+        data:{
+            email:email,
+            password:password 
+        }
+    })
+    } catch (error) {
+        res.json({
+            success: false,
+            message:error.message
+        })
+    }
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+})
