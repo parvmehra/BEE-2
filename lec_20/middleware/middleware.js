91d05667c10bfb81afbefab1ffa7773d2a89322b
Parv Mehra <156988867+parvmehra@users.noreply.github.com>
2025-08-21 15:23:27 +0530

    l20 mid

---
+++ lec_20/middleware/middleware.js
@@ -0,0 +1,35 @@
+function m1(req,res,next) {
+    console.log("running middleware 1");
+    req.user={
+        id:1,
+        name:"sahil"
+    };
+    next();
+}
+
+function m2(req,res,next) {
+    console.log("runnning middleware 2");
+    console.log(req.user);
+    next();
+}
+
+function checkAdmin(req,res,next) {
+    console.log("running chekAdmin middleware");
+    
+    let {name} = req.query;
+    if(name=="sahil"){
+        req.isAdmin = true;
+       return next();
+    }
+
+    return res.json({
+        success: false,
+        message : "you are not an admin"
+    })
+
+    
+}
+
+module.exports.m1 = m1;
+module.exports.m2 = m2;
+module.exports.checkAdmin = checkAdmin;
---
+++ lec_20/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const {m1, m2, checkAdmin} = require('./middleware/middleware.js');
+const app = express();
+
+const PORT = 3069;
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(__dirname+"/public"));
+
+app.use(m1);
+
+
+app.get('/home',(req,res,next)=>{
+    console.log("running controller home")
+    res.json({
+        succes : true,
+        message : "welcome to the home page"
+    })
+    next();
+});
+app.use(m2);
+
+app.get('/dashboard',checkAdmin,(req,res)=>{
+    if(req.isAdmin){
+       return res.json({
+            success : true,
+            message : "welcome to the dashboard",
+        })
+    }
+})
+app.listen(PORT,()=>{
+    console.log("server is running on port", PORT);
+    
+})
